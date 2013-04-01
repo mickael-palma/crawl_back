e60c01a59796c0fe10a95046efe7248b97d351bf
powermike <mickael@spypp.com>
2013-04-01 22:13:17 +0200

    Change require statement

--- lib/crawl_back.rb
+++ lib/crawl_back.rb
@@ -1,7 +1,7 @@
-require_relative "crawl_back/version"
-require_relative "crawl_back/website"
-require_relative "crawl_back/url"
-require_relative "crawl_back/search"
+require "crawl_back/version"
+require "crawl_back/website"
+require "crawl_back/url"
+require "crawl_back/search"
 
 module CrawlBack
   # Your code goes here...
