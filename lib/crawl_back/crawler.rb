2aff07c77aa14fb42ffeeff1067adf66d3bdd1e8
powermike <mickael@spypp.com>
2013-04-06 15:19:23 +0200

    refactor search form

--- lib/crawl_back/crawler.rb
+++ lib/crawl_back/crawler.rb
@@ -2,15 +2,20 @@ require 'mechanize'
 
 module CrawlBack
   class CRAWLER
-    
-    attr_accessor  :agent
-    
-    def initialize(url)
+
+    attr_accessor :agent, :field_options, :form_options, :term
+
+    def initialize(url, options={})
       @url = url
 
       # initialize agent
       @agent = Mechanize.new
       @agent.user_agent = 'CrawlBack'
+
+      # search form details
+      @term          = options[:term]
+      @field_options = options[:field_options]
+      @form_options  = options[:form_options]
     end
     
     def random_user_agent
@@ -20,5 +25,16 @@ module CrawlBack
     def get
       @agent.get(@url)
     end
+
+    def search_form
+      get.form_with @form_options
+    end
+
+    def submit_search_form
+      search_form.field_with(@field_options).value = @term
+      @agent.submit search_form
+    end
+    alias_method  :results, :submit_search_form
+    
   end
 end
--- test/lib/crawl_back/test_crawler.rb
+++ test/lib/crawl_back/test_crawler.rb
@@ -3,6 +3,9 @@ require_relative '../../test_helper'
 describe CrawlBack::CRAWLER do
   before do
     @crawler = CrawlBack::CRAWLER.new('http://www.google.com')
+    @crawler.term          = "test"
+    @crawler.field_options = {name: 'q'} # 'q' is the google form textfield name
+    @crawler.form_options  = {name: 'f'} # 'f' is the google form name
   end
 
   it 'must get a webpage' do
@@ -13,8 +16,18 @@ describe CrawlBack::CRAWLER do
     @crawler.agent.user_agent.must_equal 'CrawlBack'
   end
 
-  it 'must have a user agent from AGENT_ALIASES' do
+  it 'must have a random user agent from AGENT_ALIASES' do
     Mechanize::AGENT_ALIASES.keys.must_include @crawler.random_user_agent
+  end
+
+  describe "the search must use the websiste search form" do
+    it "must be a Mechanize form" do
+      @crawler.search_form.must_be_instance_of Mechanize::Form
+    end
+
+    it "must submit the search form" do
+      @crawler.submit_search_form.must_be_instance_of Mechanize::Page
+    end
   end
   
 end
