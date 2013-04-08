bb3541acb2ac85f8b49af1c1ce14ac49812fa1f6
powermike <mickael@spypp.com>
2013-04-08 08:57:45 +0200

    little define_method refactor

--- lib/crawl_back/website_options.rb
+++ lib/crawl_back/website_options.rb
@@ -11,43 +11,38 @@ module CrawlBack
 
     def config(&block)
       config = Config.new &block
-      @options[:config] = { base_url: config.options[:base_url] }
+      @options[:config] = { base_url: config.base_url }
     end
 
     def search_form(&block)
       form = SearchForm.new &block
-      @options[:search_form]  = { form_name: form.options[:form_name], text_field: form.options[:text_field] }
+      @options[:search_form]  = { form_name: form.form_name, text_field: form.text_field }
     end
   end
 
   class Config < WEBSITE_OPTIONS
-    
-    attr_accessor :options
-
     def initialize(&block)
-      @options = {}
-
       instance_eval &block
     end
 
     %w(base_url).each do |meth|
-      define_method(meth) { |arg| @options[meth.to_sym] = arg }
+      define_method(meth) do |*args| 
+        args.blank? ? instance_variable_get("@#{meth}") : instance_variable_set("@#{meth}", args.first)
+      end
     end
 
   end
 
   class SearchForm < WEBSITE_OPTIONS
 
-    attr_accessor :options
-
     def initialize(&block)
-      @options = {}
-
       instance_eval &block
     end
 
     %w(form_name text_field).each do |meth|
-      define_method(meth) { |arg| @options[meth.to_sym] = arg }
+      define_method(meth) do |*args| 
+        args.blank? ? instance_variable_get("@#{meth}") : instance_variable_set("@#{meth}", args.first)
+      end
     end
 
   end    
